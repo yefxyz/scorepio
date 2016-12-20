9991a6ede364a940c8c41fee40a7ca2ed3b68a45
yefxyz <yefxyz@outlook.com>
2016-12-20 12:23:29 +0800

    BaseDao

--- src/main/java/com/andre/core/repository/BaseDao.java
+++ src/main/java/com/andre/core/repository/BaseDao.java
@@ -1,5 +1,10 @@
 package com.andre.core.repository;
 
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andre.core.CoreConstants;
 import com.andre.core.model.DataObject;
 
 /**
@@ -10,11 +15,61 @@ import com.andre.core.model.DataObject;
 public interface BaseDao {
 
 	/**
+	 * 获取Logger。
+	 * 
+	 * @return
+	 */
+	Logger getLogger();
+
+	/**
+	 * 获取Context。
+	 * 
+	 * @return
+	 */
+	ApplicationContext getApplicationContext();
+
+	/**
+	 * 获取指定数据对象的RedisDao。
+	 * 
+	 * @param data
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	default <T extends DataObject> GenericRedisDao<T> getRedisDao(T data) {
+		try {
+			String prefix = data.getClass().getSimpleName().toLowerCase();
+			String redisDaoName = prefix + CoreConstants.REDIS_DAO_POSTFIX;
+			return (GenericRedisDao<T>) getApplicationContext().getBean(redisDaoName);
+		} catch (Exception e) {
+			getLogger().error("Can not find matched redis dao.", e);
+		}
+		return null;
+	}
+
+	/**
+	 * 获取指定数据对象的JpaRepository。
+	 * 
+	 * @param data
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	default <T extends DataObject> JpaRepository<T, Long> getJAPRepository(T data) {
+		try {
+			String prefix = data.getClass().getSimpleName().toLowerCase();
+			String jpaRepositoryName = prefix + CoreConstants.JPA_REPOSITORY_POSTFIX;
+			return (JpaRepository<T, Long>) getApplicationContext().getBean(jpaRepositoryName);
+		} catch (Exception e) {
+			getLogger().error("Can not find matched jpa repository.", e);
+		}
+		return null;
+	}
+
+	/**
 	 * 保存数据对象。
 	 * 
 	 * @param data
 	 */
-	public <T extends DataObject> void save(T data);
+	<T extends DataObject> void save(T data);
 
 	/**
 	 * 查找获取数据对象。
@@ -22,6 +77,6 @@ public interface BaseDao {
 	 * @param key
 	 * @return
 	 */
-	public <T extends DataObject> T find(String key);
+	<T extends DataObject> T find(String key);
 
 }
--- src/main/java/com/andre/core/repository/CacheAsideBaseDaoImpl.java
+++ src/main/java/com/andre/core/repository/CacheAsideBaseDaoImpl.java
@@ -7,7 +7,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.andre.core.CoreConstants;
 import com.andre.core.model.DataObject;
 
 /**
@@ -20,23 +19,19 @@ public class CacheAsideBaseDaoImpl implements BaseDao {
 
 	private static ApplicationContext ctx;
 
-	protected Logger logger = LogManager.getLogger(getClass());
-
 	@Autowired
 	public void setCtx(ApplicationContext ctx) {
 		CacheAsideBaseDaoImpl.ctx = ctx;
 	}
 
-	@SuppressWarnings("unchecked")
-	public <T extends DataObject> GenericRedisDao<T> getRedisDao(T data) {
-		try {
-			String prefix = data.getClass().getSimpleName().toLowerCase();
-			String redisDaoName = prefix + CoreConstants.REDIS_DAO_POSTFIX;
-			return (GenericRedisDao<T>) ctx.getBean(redisDaoName);
-		} catch (Exception e) {
-			logger.error("Can not find matched redis dao.", e);
-		}
-		return null;
+	@Override
+	public Logger getLogger() {
+		return LogManager.getLogger(getClass());
+	}
+
+	@Override
+	public ApplicationContext getApplicationContext() {
+		return ctx;
 	}
 
 	@Transactional
